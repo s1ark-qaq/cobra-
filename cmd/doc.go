80c4649a1cb700c822c4158bcd265a5a16bfb550
s1ark-qaq <1575628b1t@gmail.com>
2025-11-30 23:18:27 +0800

    增加注释命令

---
+++ cmd/doc.go
@@ -0,0 +1,18 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	docd "github.com/spf13/cobra/doc"
+)
+
+func init() {
+	rootCmd.AddCommand(doc)
+}
+
+var doc = &cobra.Command{
+	Use:   "doc",
+	Short: "Generate documentation",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return docd.GenMarkdownTree(rootCmd, "./docs")
+	},
+}
